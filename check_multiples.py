1b8554cd86553a76ce2cfd437f79138233465fbf
Will Browning <wdbrwnng@memphis.edu>
2024-02-05 15:16:26 -0600

    Update Output test

--- check_multiples.py
+++ check_multiples.py
@@ -1,7 +1,18 @@
+import random
+
 number = 75
 divisor = 5
+def check_multiple(n, d):
+    if number % divisor == 0:
+        print(f"{number} is divisible by {divisor}")
+    else:
+        print(f"{number} is not divisible by {divisor}")
 
-if number % divisor == 0:
-    print(f"{number} is divisible by {divisor}")
-else:
-    print(f"{number} is not divisible by {divisor}")
+
+number = random.randint(1, 100)
+divisor = random.randint(1,10)
+
+check_multiple(number, divisor)
+
+print("hey")
+
